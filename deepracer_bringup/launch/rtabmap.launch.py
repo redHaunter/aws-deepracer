417d1cad7d0a74e7e25c8fc5abe39970d1a98de0
redHaunter <m.reza.ramezani8@gmail.com>
2023-09-15 03:21:33 +0330

    fixing framename

--- deepracer_bringup/launch/rtabmap.launch.py
+++ deepracer_bringup/launch/rtabmap.launch.py
@@ -7,15 +7,32 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    parameters = [
+    slam_parameters = [
         {
             "use_sim_time": True,
-            "subscribe_stereo": True,
-            "subscribe_depth": False,
-            "frame_id": "base_link",
-            "odom_frame_id": "odom",
+            "subscribe_stereo": False,
+            "subscribe_depth": True,
+            "frame_id": "camera_link",
             "approx_sync": True,
             "queue_size": 200,
+            "publish_tf": True,
+            # "subscribe_scan": True,
+        }
+    ]
+    slam_remmapings = [
+        # ("right/image_rect", "/zed_camera/right/image_rect"),
+        # ("left/image_rect", "/zed_camera/left/image_rect"),
+        # ("right/camera_info", "/zed_camera/right/camera_info"),
+        # ("left/camera_info", "/zed_camera/left/camera_info"),
+        ("rgb/image", "/zed_camera/left/image_rect_color"),
+        ("rgb/camera_info", "/zed_camera/left/camera_info"),
+        ("depth/image", "/image_raw"),
+
+        
+    ]
+    depth_params = [
+        {
+            "fixed_frame_id": "odom",
         }
     ]
     odom_params = [
@@ -39,6 +56,10 @@ def generate_launch_description():
         ("right/camera_info", "/zed_camera/right/camera_info"),
         ("left/camera_info", "/zed_camera/left/camera_info"),
     ]
+    depth_remmapings = [
+        ("cloud", "/points2"),
+        ("camera_info", "/zed_camera/left/camera_info"),
+    ]
     odometry_remmapings = [
         ("right/image_rect", "/zed_camera/right/image_rect"),
         ("left/image_rect", "/zed_camera/left/image_rect"),
@@ -46,12 +67,7 @@ def generate_launch_description():
         ("left/camera_info", "/zed_camera/left/camera_info"),
     ]
     # -r right/image_rect:=/zed_camera/right/image_rect -r left/image_rect:=/zed_camera/left/image_rect -r right/camera_info:=/zed_camera/right/camera_info -r left/camera_info:=/zed_camera/left/camera_info
-    slam_remmapings = [
-        ("right/image_rect", "/zed_camera/right/image_rect_color"),
-        ("left/image_rect", "/zed_camera/left/image_rect_color"),
-        ("right/camera_info", "/zed_camera/right/camera_info"),
-        ("left/camera_info", "/zed_camera/left/camera_info"),
-    ]
+
     stereo_image_proc_dir = get_package_share_directory("stereo_image_proc")
     stereo_image_proc_launcher = IncludeLaunchDescription(
         launch_description_source=PythonLaunchDescriptionSource(
@@ -62,18 +78,36 @@ def generate_launch_description():
             "approximate_sync": "True",
             "left_namespace": "/zed_camera/left",
             "right_namespace": "/zed_camera/right",
+            "target_frame_id": "/zed_camera_link_leftcam",
+            "avoid_point_cloud_padding": "True",
         }.items(),
     )
 
     return LaunchDescription(
         [
             launch_ros.actions.Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '-1.5707963267948966', '0', '-1.5707963267948966', 'base_link', 'camera_link'],
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                output="screen",
+                arguments=[
+                    "0",
+                    "0",
+                    "0",
+                    "-1.5707963267948966",
+                    "0",
+                    "-1.5707963267948966",
+                    "base_link",
+                    "camera_link",
+                ],
             ),
             stereo_image_proc_launcher,
+            launch_ros.actions.Node(
+                package="rtabmap_util",
+                executable="pointcloud_to_depthimage",
+                output="screen",
+                parameters=depth_params,
+                remappings=depth_remmapings,
+            ),
             # launch_ros.actions.Node(
             #     package="rtabmap_sync",
             #     executable="stereo_sync",
@@ -113,27 +147,26 @@ def generate_launch_description():
             #     ],
             #     # remappings=odometry_remmapings,
             # ),
-            # launch_ros.actions.Node(
-            #     package="rtabmap_slam",
-            #     executable="rtabmap",
-            #     output="screen",
-            #     arguments=[
-            #         "-d",
-            #         "--Vis/CorFlowMaxLevel",
-            #         "5",
-            #         "--Stereo/MaxDisparity 200",
-            #     ],
-            #     parameters=parameters,
-            #     remappings=slam_remmapings,
-            # ),
-            # launch_ros.actions.Node(
-            #     package="rtabmap_viz",
-            #     executable="rtabmap_viz",
-            #     output="screen",
-            #     parameters=parameters,
-            #     remappings=slam_remmapings,
-            # ),
+            launch_ros.actions.Node(
+                package="rtabmap_slam",
+                executable="rtabmap",
+                output="screen",
+                arguments=[
+                    "-d",
+                    "--Vis/CorFlowMaxLevel",
+                    "5",
+                    "--Stereo/MaxDisparity", 
+                    "200",
+                ],
+                parameters=slam_parameters,
+                remappings=slam_remmapings,
+            ),
+            launch_ros.actions.Node(
+                package="rtabmap_viz",
+                executable="rtabmap_viz",
+                output="screen",
+                parameters=slam_parameters,
+                remappings=slam_remmapings,
+            ),
         ]
     )
-
-
